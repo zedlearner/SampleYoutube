412dceae31684a6e414d44a05542b84b1b87869c
Zeeyad <zedlearner@gmail.com>
2017-10-15 08:27:18 +0300

    Implemented: Settings menu

---
+++ YoutubeSample/Setting.swift
@@ -0,0 +1,13 @@
+//
+//  Setting.swift
+//  YoutubeSample
+//
+//  Created by Admin on 09/07/1939 Saka.
+//  Copyright © 1939 Saka zedlearner. All rights reserved.
+//
+
+import UIKit
+
+class Setting: NSObject {
+
+}
---
+++ YoutubeSample/SettingCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingCell.swift
+//  YoutubeSample
+//
+//  Created by Admin on 09/07/1939 Saka.
+//  Copyright © 1939 Saka zedlearner. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
